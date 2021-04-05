30cb61bfa619ac90720dcd75ea12b975fb2d2e7a
Mike Solomon <mike@meeshkan.com>
2021-04-05 18:12:23 +0300

    Adds move

---
+++ test/Move.purs
@@ -0,0 +1,192 @@
+module Test.Move where
+
+import Prelude
+
+import Type.Data.Peano (Succ, Z)
+import Type.Proxy (Proxy(..))
+import WAGS as W
+
+data MyGain
+
+data MySinOsc
+
+moveTest0 ::
+  W.AudioUnitRef W.D1 ->
+  W.Frame Unit Void
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D1 (W.PtrListCons W.D2 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D1 (W.PtrListCons W.D2 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    Unit
+moveTest0 ref = W.move ref (Proxy :: _ Z) (Proxy :: _ Z)
+
+moveTest1 ::
+  W.AudioUnitRef W.D1 ->
+  W.Frame Unit Void
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D1 (W.PtrListCons W.D2 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D1 (W.PtrListCons W.D2 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    Unit
+moveTest1 ref = W.move ref ref (Proxy :: _ Z)
+
+moveTest2 ::
+  W.AudioUnitRef W.D1 ->
+  W.Frame Unit Void
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D1 (W.PtrListCons W.D2 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D2 (W.PtrListCons W.D1 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    Unit
+moveTest2 ref = W.move ref ref (Proxy :: _ (Succ Z))
+
+moveTest3 ::
+  W.AudioUnitRef W.D1 ->
+  W.Frame Unit Void
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D1 (W.PtrListCons W.D2 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D2 (W.PtrListCons W.D0 (W.PtrListCons W.D1 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    Unit
+moveTest3 ref = W.move ref ref (Proxy :: _ (Succ (Succ Z)))
+
+moveTest4 ::
+  W.AudioUnitRef W.D1 ->
+  W.Frame Unit Void
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D1 (W.PtrListCons W.D2 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D2 (W.PtrListCons W.D1 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    Unit
+moveTest4 ref = W.move ref (Proxy :: _ (Succ Z)) (Proxy :: _ Z)
+
+moveTest5 ::
+  W.AudioUnitRef W.D1 ->
+  W.AudioUnitRef W.D2 ->
+  W.Frame Unit Void
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D1 (W.PtrListCons W.D2 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    ( W.UniverseC W.D3
+        ( W.GraphC
+            ( W.NodeC (W.TGain W.D1)
+                (W.ManyEdges W.D2 (W.PtrListCons W.D1 (W.PtrListCons W.D0 W.PtrListNil)))
+            )
+            ( W.NodeListCons
+                (W.NodeC (W.THighpass W.D2) (W.SingleEdge W.D0))
+                (W.NodeListCons (W.NodeC (W.TSinOsc W.D0) W.NoEdge) W.NodeListNil)
+            )
+        )
+        W.SkolemListNil
+    )
+    Unit
+moveTest5 ref ref2 = W.move ref ref2 (Proxy :: _ Z)
