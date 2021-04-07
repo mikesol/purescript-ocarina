8c8a7a0f262a7da488b7025b7bc1855a7813a7b0
Mike Solomon <mike@meeshkan.com>
2021-04-07 12:58:12 +0300

    Sorts alphabetically

--- src/WAGS/Graph/Constructors.purs
+++ src/WAGS/Graph/Constructors.purs
@@ -1,87 +1,92 @@
 module WAGS.Graph.Constructors where
 
-
-
 import Type.Proxy (Proxy)
 
-data OversampleNone = OversampleNone
-data OversampleTwoX = OversampleTwoX
-data OversampleFourX = OversampleFourX
+-- for waveshaper
+data OversampleNone
+  = OversampleNone
 
-data WaveShaper (s :: Symbol) a b
-  = WaveShaper (Proxy s) a b
+data OversampleTwoX
+  = OversampleTwoX
 
-data SinOsc a
-  = SinOsc a
+data OversampleFourX
+  = OversampleFourX
 
-data SawtoothOsc a
-  = SawtoothOsc a
-
-data TriangleOsc a
-  = TriangleOsc a
-
-data PeriodicOsc a
-  = PeriodicOsc a
-
-data SquareOsc a
-  = SquareOsc a
-
-data Dup a b
-  = Dup a b
-
-data Lowpass a b c
-  = Lowpass a b c
-
-data Highpass a b c
-  = Highpass a b c
-
-data Lowshelf a b c
-  = Lowshelf a b c
-
-data Highshelf a b c
-  = Highshelf a b c
+--
+data Allpass a b c
+  = Allpass a b c
 
 data Bandpass a b c
   = Bandpass a b c
 
-data Peaking a b c d
-  = Peaking a b c d
-
-data Notch a b c
-  = Notch a b c
-
-data Allpass a b c
-  = Allpass a b c
-
-data Gain a b
-  = Gain a b
-
-data Speaker a
-  = Speaker a
-
-data Microphone (s :: Symbol)
-  = Microphone (Proxy s)
-
-data PlayBuf (s :: Symbol) a
-  = PlayBuf (Proxy s) Number a
-
-data LoopBuf (s :: Symbol) a
-  = LoopBuf (Proxy s) a Number Number
+data Constant a
+  = Constant a
 
 data Convolver (s :: Symbol) a
   = Convolver (Proxy s) a
 
+data Delay a b
+  = Delay a b
+
+data Dup a b
+  = Dup a b
+
 data DynamicsCompressor a b c d e f
   = DynamicsCompressor a b c d e f
+
+data Gain a b
+  = Gain a b
+
+data Highpass a b c
+  = Highpass a b c
+
+data Highshelf a b c
+  = Highshelf a b c
+
+data LoopBuf (s :: Symbol) a
+  = LoopBuf (Proxy s) a Number Number
+
+data Lowpass a b c
+  = Lowpass a b c
+
+data Lowshelf a b c
+  = Lowshelf a b c
+
+data Microphone (s :: Symbol)
+  = Microphone (Proxy s)
+
+data Notch a b c
+  = Notch a b c
+
+data Peaking a b c d
+  = Peaking a b c d
+
+data PeriodicOsc a
+  = PeriodicOsc a
+
+data PlayBuf (s :: Symbol) a
+  = PlayBuf (Proxy s) Number a
+
+data Recorder (s :: Symbol) a
+  = Recorder (Proxy s) a
+
+data SawtoothOsc a
+  = SawtoothOsc a
+
+data SinOsc a
+  = SinOsc a
+
+data Speaker a
+  = Speaker a
+
+data SquareOsc a
+  = SquareOsc a
 
 data StereoPanner a b
   = StereoPanner a b
 
-data Constant a
-  = Constant a
+data TriangleOsc a
+  = TriangleOsc a
 
-data Delay a b
-  = Delay a b
-
-data Recorder (s :: Symbol) a
-  = Recorder (Proxy s) a
+data WaveShaper (s :: Symbol) a b
+  = WaveShaper (Proxy s) a b
