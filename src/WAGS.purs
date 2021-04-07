edb6aac59e04e58cb3397b313314c40a7f8a17d6
Mike Solomon <mike@meeshkan.com>
2021-04-07 20:27:22 +0300

    Constructors

--- src/WAGS.purs
+++ src/WAGS.purs
@@ -40,7 +40,7 @@ import WAGS.Graph.Decorators (Decorated, class Decorate, class MakeDecorators, D
 import WAGS.Graph.Parameter (AudioParameter(..), AudioParameter', AudioParameterTransition(..), defaultParam, param)
 import WAGS.Control.Functions (branch, env, freeze, loop, makeScene, start, universe, (@>), (@|>))
 import WAGS.Control.Types (AudioState, AudioState', Frame, Frame0, InitialFrame, InitialUniverse, Scene, oneFrame, oneFrame')
-import WAGS.Universe.AudioUnit (class AudioUnitEq, class GetPointer, AudioUnit, AudioUnitCons, AudioUnitList, AudioUnitNil, AudioUnitRef, TGain, THighpass, TSinOsc, TSpeaker)
+import WAGS.Universe.AudioUnit (class AudioUnitEq, class GetPointer, AudioUnit, AudioUnitCons, AudioUnitList, AudioUnitNil, AudioUnitRef(..), TAllpass, TBandpass, TConstant, TConvolver, TDelay, TDup, TDynamicsCompressor, TGain, THighpass, THighshelf, TLoopBuf, TLowpass, TLowshelf, TMicrophone, TNotch, TPeaking, TPeriodicOsc, TPlayBuf, TRecorder, TSawtoothOsc, TSinOsc, TSpeaker, TSquareOsc, TStereoPanner, TTriangleOsc, TWaveShaper)
 import WAGS.Universe.Bin (class AllZerosToNull, class Beq, class BinEq, class BinSub, class BinSub', class BinSucc, class BinToInt, class PtrListKeepSingleton, class RemoveTrailingZeros, type (+:), Bc, Bin, BinL, Bn, D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, I, O, Ptr, PtrList, PtrListCons, PtrListNil, toInt', toInt'')
 import WAGS.Universe.EdgeProfile (EdgeProfile, ManyEdges, NoEdge, SingleEdge)
 import WAGS.Universe.Graph (class GraphToNodeList, Graph, GraphC, InitialGraph)
