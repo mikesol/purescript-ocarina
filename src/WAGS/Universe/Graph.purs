c9fbfce2fb540c753f1af6b012554c03a6cf5c05
Mike Solomon <mike@meeshkan.com>
2021-04-17 17:58:20 +0300

    Adds documentation for graphs

--- src/WAGS/Universe/Graph.purs
+++ src/WAGS/Universe/Graph.purs
@@ -2,13 +2,15 @@ module WAGS.Universe.Graph where
 
 import WAGS.Universe.Node (Node, NodeList, NodeListCons, NodeListNil)
 
+-- | A graph of audio nodes.
 data Graph
 
--- non empty
+-- | The sole constructor of a graph. A graph is just a non-empty node list.
 foreign import data GraphC :: Node -> NodeList -> Graph
 
 foreign import data InitialGraph :: Graph
 
+-- | Converts a graph to a node list and back again.
 class GraphToNodeList (graph :: Graph) (nodeList :: NodeList) | graph -> nodeList, nodeList -> graph
 
 instance graphToNodeList :: GraphToNodeList (GraphC node nodeList) (NodeListCons node nodeList)
--- src/WAGS/Universe/Node.purs
+++ src/WAGS/Universe/Node.purs
@@ -3,21 +3,29 @@ module WAGS.Universe.Node where
 import WAGS.Universe.AudioUnit (AudioUnit)
 import WAGS.Universe.EdgeProfile (EdgeProfile)
 
+-- | A single audio node in the graph.
 data Node
 
+-- | The sole constructor for an audio node. It is comprised of two distinct parts:
+-- | - `AudioUnit` - the name of the node (ie a highpass filter, a sine-wave oscillator, etc) along with its pointer
+-- | - `EdgeProfile` - incoming edges into the node
 foreign import data NodeC :: AudioUnit -> EdgeProfile -> Node
 
+-- | A list of audio nodes.
 data NodeList
 
+-- | Cons for the audio node list.
 foreign import data NodeListCons :: Node -> NodeList -> NodeList
 
+-- | Nil for the audio node list.
 foreign import data NodeListNil :: NodeList
 
 infixr 5 type NodeListCons as /:
 
 infixr 5 type NodeC as /->
 
-
+-- | This class acts as Alt for a `NodeList` with a supplemental assertion that the chosen list
+-- | must contain at most one node.
 class NodeListKeepSingleton (nodeListA :: NodeList) (nodeListB :: NodeList) (nodeListC :: NodeList) | nodeListA nodeListB -> nodeListC
 
 instance nodeListKeepSingletonNil :: NodeListKeepSingleton NodeListNil NodeListNil NodeListNil
@@ -26,6 +34,7 @@ instance nodeListKeepSingletonL :: NodeListKeepSingleton (NodeListCons a NodeLis
 
 instance nodeListKeepSingletonR :: NodeListKeepSingleton NodeListNil (NodeListCons a NodeListNil) (NodeListCons a NodeListNil)
 
+-- | Class to get the audio unit from a node.
 class GetAudioUnit (node :: Node) (au :: AudioUnit) | node -> au
 
 instance getAudioUnitNodeC :: GetAudioUnit (NodeC au ep) au
