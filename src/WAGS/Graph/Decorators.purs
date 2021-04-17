905098ed1a9e13b64959ee374cf96926148a7294
Mike Solomon <mike@meeshkan.com>
2021-04-17 22:46:59 +0300

    Adds documentation for decorators

--- src/WAGS/Graph/Decorators.purs
+++ src/WAGS/Graph/Decorators.purs
@@ -12,12 +12,106 @@ import Data.Tuple (Tuple)
 import WAGS.Graph.Constructors as CTOR
 import WAGS.Rendered (Oversample(..))
 
-type Decorated a
-  = forall b c. Decorate a b c => { | b } /\ { | c }
+-- A newtype for a decorator, isomorphic to `Exists f` where `f` is a type constructor of type `Type -> Type`.
+newtype Decorating f
+  = Decorating (Decorating' f)
 
+-- | The internal type of decorator.
+type Decorating' f
+  = forall a. a -> f a
+
+-- | Retrieves the type constructor from decorating, isomorphic to `runExists`.
+dk :: forall f. Decorating f -> Decorating' f
+dk (Decorating f) = f
+
+-- | A class for the automatic generation of cursors
+-- | Given an input row `a` of decorators, outputs a row `b` of `Identity` decorators as well as a Row `c` of `Focus` decorators for each key in `a`.
+-- | For example:
+-- |
+-- | ```purescript
+-- | decorators ::
+-- |   Tuple
+-- |     { d0: Decorating Identity, d1: Decorating Identity }
+-- |     { d0: { d0: Decorating Focus, d1: Decorating Identity }
+-- |     , d1: { d0: Decorating Identity, d1: Decorating Focus }
+-- |     }
+-- | decorators = decorate \{d0, d1} ->
+-- |   Speaker (dk d0 SinOsc 440.0 /\ dk d1 SinOsc 880.0 /\ unit)
+-- | ```
+-- |
+-- | Check out `test/Ops.purs` for an example of decorators in use.
 class Decorate (a :: Row Type) (b :: Row Type) (c :: Row Type) | a -> b c where
   decorate :: forall d. ({ | a } -> d) -> { | b } /\ { | c }
 
+-- Used to focus on a cursor.
+data Focus a
+  = Focus a
+
+-- Class to determine if a type is audio.
+class IsAudio (audio :: Type)
+
+instance isAudioAllpass :: IsAudio (CTOR.Allpass a b c)
+else instance isAudioBandpass :: IsAudio (CTOR.Bandpass a b c)
+else instance isAudioConstant :: IsAudio (CTOR.Constant a)
+else instance isAudioConvolver :: IsAudio (CTOR.Convolver a b)
+else instance isAudioDelay :: IsAudio (CTOR.Delay a b)
+else instance isAudioDup :: IsAudio (CTOR.Dup a b)
+else instance isAudioDynamicsCompressor :: IsAudio (CTOR.DynamicsCompressor a b c d e f)
+else instance isAudioGain :: IsAudio (CTOR.Gain a b)
+else instance isAudioHighpass :: IsAudio (CTOR.Highpass a b c)
+else instance isAudioHighshelf :: IsAudio (CTOR.Highshelf a b c)
+else instance isAudioLoopBuf :: IsAudio (CTOR.LoopBuf a b)
+else instance isAudioLowpass :: IsAudio (CTOR.Lowpass a b c)
+else instance isAudioLowshelf :: IsAudio (CTOR.Lowshelf a b c)
+else instance isAudioMicrophone :: IsAudio CTOR.Microphone
+else instance isAudioNotch :: IsAudio (CTOR.Notch a b c)
+else instance isAudioPeaking :: IsAudio (CTOR.Peaking a b c b)
+else instance isAudioPeriodicOsc :: IsAudio (CTOR.PeriodicOsc a b)
+else instance isAudioPlayBuf :: IsAudio (CTOR.PlayBuf a b)
+else instance isAudioRecorder :: IsAudio (CTOR.Recorder a b)
+else instance isAudioSawtoothOsc :: IsAudio (CTOR.SawtoothOsc a)
+else instance isAudioSinOsc :: IsAudio (CTOR.SinOsc a)
+else instance isAudioSpeaker :: IsAudio (CTOR.Speaker a)
+else instance isAudioSquareOsc :: IsAudio (CTOR.SquareOsc a)
+else instance isAudioStereoPanner :: IsAudio (CTOR.StereoPanner a b)
+else instance isAudioTriangleOsc :: IsAudio (CTOR.TriangleOsc a)
+else instance isAudioWaveShaper :: IsAudio (CTOR.WaveShaper a b c)
+else instance isAudioProxy :: IsAudio (Proxy s)
+else instance isAudioFofAudio :: IsAudio i => IsAudio (f i)
+
+-- | Class to determine if a type is audio or multiple audios.
+class IsMultiAudio (audio :: Type)
+
+instance isMultiAudioUnit :: IsMultiAudio Unit
+else instance isMultiAudioTuple :: (IsMultiAudio a, IsMultiAudio b) => IsMultiAudio (Tuple a b)
+else instance isMultiAudioAudio :: IsAudio a => IsMultiAudio a
+
+-- | Class to determine if a type is audio or a closure producing audio.
+class IsAudioOrF (audioOrF :: Type)
+
+instance isAudioOrFProxy :: IsAudio a => IsAudioOrF (Proxy s -> a)
+else instance isAudioOrFAudio :: IsAudio a => IsAudioOrF a
+
+-- | Class to determine if a type is audio, multiple audios, or a closure producing audio or multiple audios.
+class IsMultiAudioOrF (audioOrF :: Type)
+
+instance isMultiAudioOrFProxy :: IsMultiAudio a => IsMultiAudioOrF (Proxy s -> a)
+else instance isMultiAudioOrFAudio :: IsMultiAudio a => IsMultiAudioOrF a
+
+-- | Class to determine if a type is an oversample directive.
+class IsOversample oversample where
+  reflectOversample :: oversample -> Oversample
+
+instance isOversampleNone :: IsOversample CTOR.OversampleNone where
+  reflectOversample _ = None
+
+instance isOversampleTwoX :: IsOversample CTOR.OversampleTwoX where
+  reflectOversample _ = TwoX
+
+instance isOversampleFourX :: IsOversample CTOR.OversampleFourX where
+  reflectOversample _ = FourX
+
+-- | Internal helper class to construct decorators.
 class MakeDecorators (rl :: RL.RowList Type) b | rl -> b where
   makeDecorators :: Proxy rl -> { | b }
 
@@ -27,6 +121,7 @@ instance makeDecoratorsRLCons :: (IsSymbol sym, Lacks sym rest, Cons sym (Decora
 instance makeDecoratorsRLNil :: MakeDecorators RL.Nil () where
   makeDecorators _ = {}
 
+-- | Internal helper class to construct focusing decorators.
 class MakeFocusing (rl :: RL.RowList Type) (b :: Row Type) (c :: Row Type) | rl b -> c where
   makeFocusing :: Proxy rl -> { | b } -> { | c }
 
@@ -41,102 +136,3 @@ instance decorateDecorating :: (RL.RowToList a rl, MakeDecorators rl b, MakeFocu
     where
     idy = makeDecorators (Proxy :: _ rl)
 
-type Decorating' f
-  = forall a. a -> f a
-
-newtype Decorating f
-  = Decorating (Decorating' f)
-
-dk :: forall f. Decorating f -> (Decorating' f)
-dk (Decorating f) = f
-
-data Focus a
-  = Focus a
-
-class IsAudio (audio :: Type)
-
-instance isAudioAllpass :: IsAudio (CTOR.Allpass a b c)
-
-instance isAudioBandpass :: IsAudio (CTOR.Bandpass a b c)
-
-instance isAudioConstant :: IsAudio (CTOR.Constant a)
-
-instance isAudioConvolver :: IsAudio (CTOR.Convolver a b)
-
-instance isAudioDelay :: IsAudio (CTOR.Delay a b)
-
-instance isAudioDup :: IsAudio (CTOR.Dup a b)
-
-instance isAudioDynamicsCompressor :: IsAudio (CTOR.DynamicsCompressor a b c d e f)
-
-instance isAudioGain :: IsAudio (CTOR.Gain a b)
-
-instance isAudioHighpass :: IsAudio (CTOR.Highpass a b c)
-
-instance isAudioHighshelf :: IsAudio (CTOR.Highshelf a b c)
-
-instance isAudioLoopBuf :: IsAudio (CTOR.LoopBuf a b)
-
-instance isAudioLowpass :: IsAudio (CTOR.Lowpass a b c)
-
-instance isAudioLowshelf :: IsAudio (CTOR.Lowshelf a b c)
-
-instance isAudioMicrophone :: IsAudio CTOR.Microphone
-
-instance isAudioNotch :: IsAudio (CTOR.Notch a b c)
-
-instance isAudioPeaking :: IsAudio (CTOR.Peaking a b c b)
-
-instance isAudioPeriodicOsc :: IsAudio (CTOR.PeriodicOsc a b)
-
-instance isAudioPlayBuf :: IsAudio (CTOR.PlayBuf a b)
-
-instance isAudioRecorder :: IsAudio (CTOR.Recorder a b)
-
-instance isAudioSawtoothOsc :: IsAudio (CTOR.SawtoothOsc a)
-
-instance isAudioSinOsc :: IsAudio (CTOR.SinOsc a)
-
-instance isAudioSpeaker :: IsAudio (CTOR.Speaker a)
-
-instance isAudioSquareOsc :: IsAudio (CTOR.SquareOsc a)
-
-instance isAudioStereoPanner :: IsAudio (CTOR.StereoPanner a b)
-
-instance isAudioTriangleOsc :: IsAudio (CTOR.TriangleOsc a)
-
-instance isAudioWaveShaper :: IsAudio (CTOR.WaveShaper a b c)
-
-instance isAudioProxy :: IsAudio (Proxy s)
-
-instance isAudioIdentity :: IsAudio i => IsAudio (Identity i)
-
-instance isAudioFocus :: IsAudio f => IsAudio (Focus f)
-
-class IsMultiAudio (audio :: Type)
-
-instance isMultiAudioUnit :: IsMultiAudio Unit
-else instance isMultiAudioTuple :: (IsMultiAudio a, IsMultiAudio b) => IsMultiAudio (Tuple a b)
-else instance isMultiAudioAudio :: IsAudio a => IsMultiAudio a
-
-class IsAudioOrF (audioOrF :: Type)
-
-instance isAudioOrFProxy :: IsAudio a => IsAudioOrF (Proxy s -> a)
-else instance isAudioOrFAudio :: IsAudio a => IsAudioOrF a
-
-class IsMultiAudioOrF (audioOrF :: Type)
-
-instance isMultiAudioOrFProxy :: IsMultiAudio a => IsMultiAudioOrF (Proxy s -> a)
-else instance isMultiAudioOrFAudio :: IsMultiAudio a => IsMultiAudioOrF a
-
-class IsOversample oversample where
-  reflectOversample :: oversample -> Oversample
-
-instance isOversampleNone :: IsOversample CTOR.OversampleNone where
-  reflectOversample _ = None
-
-instance isOversampleTwoX :: IsOversample CTOR.OversampleTwoX where
-  reflectOversample _ = TwoX
-
-instance isOversampleFourX :: IsOversample CTOR.OversampleFourX where
-  reflectOversample _ = FourX
