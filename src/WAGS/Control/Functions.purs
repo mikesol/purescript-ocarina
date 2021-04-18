a4b318080967a5bc4509667248dcac4f5de32b50
Mike Solomon <mike@meeshkan.com>
2021-04-18 09:59:52 +0300

    Adds documentation for functions

--- src/WAGS/Control/Functions.purs
+++ src/WAGS/Control/Functions.purs
@@ -35,15 +35,32 @@ import WAGS.Interpret (class AudioInterpret)
 import WAGS.Universe.Universe (UniverseC)
 import WAGS.Validation (class GraphIsRenderable, class TerminalIdentityEdge)
 
+-- | The initial `Frame` that is needed to begin any `Scene`.
+-- |
+-- | ```purescript
+-- | piece :: Scene (SceneI Unit Unit) FFIAudio (Effect Unit) Frame0
+-- | piece =
+-- |   Ix.do
+-- |     start -- initial frame
+-- |     { time } <- env
+-- |     create (scene time) $> Right unit
+-- |     @> loop
+-- |         ( const
+-- |             $ Ix.do
+-- |                 { time } <- env
+-- |                 ivoid $ change (scene time)
+-- |         )
+-- | ```
+start :: forall env audio engine. InitialFrameT env audio engine Thunkable Unit
+start = unsafeFrame (pure unit)
+
+-- | The initial `FrameT` that is needed to begin any `SceneT`.
 startT ::
   forall env audio engine m.
   Monad m =>
   AudioInterpret audio engine =>
   InitialFrameT env audio engine m Unit
 startT = unsafeFrame (pure unit)
-
-start :: forall env audio engine. InitialFrameT env audio engine Thunkable Unit
-start = unsafeFrame (pure unit)
 
 initialAudioState :: forall env audio engine. env -> AudioState' env audio engine
 initialAudioState e =
@@ -54,6 +71,30 @@ initialAudioState e =
   , internalEdges: M.empty
   }
 
+-- | Make a scene. The infix operator for this operation is `@>`.
+-- |
+-- | This function uses the `GraphIsRenderable` typeclass to assert that an audio graph is renderable by the web audio engine. This means, amongst other things, that it has a unique output device (ie speaker), that it does not have any dangling units not connected to a loudspeaker, etc.
+-- |
+-- | It accepts as arguments:
+-- | - a frame to render
+-- | - a function that accepts a frame from the next moment in time (`proofB`) and returns a scene.
+-- |
+-- | From these arguments, it produces a `SceneT`.
+-- |
+-- | ```purescript
+-- | piece :: Scene (SceneI Unit Unit) FFIAudio (Effect Unit) Frame0
+-- | piece =
+-- |   Ix.do
+-- |     start
+-- |     { time } <- env
+-- |     create (scene time) $> Right unit
+-- |     @> loop -- here, @> is the infix version of `makeScene`
+-- |         ( const
+-- |             $ Ix.do
+-- |                 { time } <- env
+-- |                 ivoid $ change (scene time)
+-- |         )
+-- | ```
 makeScene ::
   forall env audio engine proofA m i currentIdx graph changeBit skolems a.
   Monad m =>
@@ -102,6 +143,73 @@ makeScene m trans = SceneT go
 
 infixr 6 makeScene as @>
 
+-- | Loops audio.
+-- |
+-- | In WAGS, a "loop" is a universe whose `changeBit` increments by 1. That means that the structure of the graph is similar (no units added, none taken away) while some or none of its internal content (ie frequencies, gains, etc) has changed. This is accomplished using the `change` family of functions in `WAGS.Change`.
+-- |
+-- | ```purescript
+-- | piece :: Scene (SceneI Unit Unit) FFIAudio (Effect Unit) Frame0
+-- | piece =
+-- |   Ix.do
+-- |     start -- initial frame
+-- |     { time } <- env
+-- |     create (scene time) $> Right unit
+-- |     @> loop -- we loop by changing the scene based on `time` in the `env`
+-- |         ( const
+-- |             $ Ix.do
+-- |                 { time } <- env
+-- |                 ivoid $ change (scene time)
+-- |         )
+-- | ```
+loop ::
+  forall env audio engine proofA i m currentIdx graph changeBit skolems edge a.
+  Monad m =>
+  AudioInterpret audio engine =>
+  TerminalIdentityEdge graph edge =>
+  GraphIsRenderable graph =>
+  ( forall proofB j.
+    a ->
+    FrameT env audio engine proofB m (UniverseC currentIdx graph j skolems)
+      (UniverseC currentIdx graph (Succ j) skolems)
+      a
+  ) ->
+  FrameT env audio engine proofA m i
+    (UniverseC currentIdx graph changeBit skolems)
+    a ->
+  SceneT env audio engine proofA m
+loop fa ma = makeScene (imap Right $ Ix.bind ma fa) (loop fa)
+
+-- | Accepts a "branch" frame for making a scene, where `Left` is a new scene and `Right` is the incoming scene with the change bit incremented by 1. Useful for the common pattern where we loop an audio graph until something in the environment changes, at which point we move on to a new graph.
+-- |
+-- | ```purescript
+-- | simpleScene =
+-- |   ( Ix.do
+-- |       start
+-- |       e <- env
+-- |       create (scene0 e) $> Right unit
+-- |   )
+-- |     @> ( branch Ix.do
+-- |           { time } <- env
+-- |           pr <- proof
+-- |           withProof pr
+-- |             $ if time < 0.3 then
+-- |                 Right
+-- |                   ( const
+-- |                       $ Ix.do
+-- |                           e <- env
+-- |                           ivoid $ change (scene0 e)
+-- |                   )
+-- |               else
+-- |                 Left
+-- |                   ( loop
+-- |                       ( const
+-- |                           $ Ix.do
+-- |                               e <- env
+-- |                               ivoid $ change (scene1 e)
+-- |                       )
+-- |                   )
+-- |       )
+-- | ```
 branch ::
   forall env audio engine proofA i m currentIdx graph changeBit skolems a.
   Monad m =>
@@ -140,24 +248,11 @@ branch mch m =
     )
     (branch mch)
 
-loop ::
-  forall env audio engine proofA i m currentIdx graph changeBit skolems edge a.
-  Monad m =>
-  AudioInterpret audio engine =>
-  TerminalIdentityEdge graph edge =>
-  GraphIsRenderable graph =>
-  ( forall proofB j.
-    a ->
-    FrameT env audio engine proofB m (UniverseC currentIdx graph j skolems)
-      (UniverseC currentIdx graph (Succ j) skolems)
-      a
-  ) ->
-  FrameT env audio engine proofA m i
-    (UniverseC currentIdx graph changeBit skolems)
-    a ->
-  SceneT env audio engine proofA m
-loop fa ma = makeScene (imap Right $ Ix.bind ma fa) (loop fa)
-
+-- | Freezes the current audio frame.
+-- |
+-- | ```purescript
+-- | scene = (start :*> create (speaker (sinOsc 440.0))) @|> freeze
+-- | ```
 freeze ::
   forall env audio engine proof m i currentIdx graph changeBit skolems x.
   Monad m =>
@@ -169,6 +264,7 @@ freeze ::
   SceneT env audio engine proof m
 freeze s = makeScene (imap Right s) freeze
 
+-- | Similar to `makeScene'`, but without the possibility to branch to a new scene. Aliased as `@|>`.
 makeScene' ::
   forall env audio engine proofA m i currentIdx graph changeBit skolems a.
   Monad m =>
@@ -188,6 +284,22 @@ makeScene' a b = makeScene (imap Right a) b
 
 infixr 6 makeScene' as @|>
 
+-- | Get the environment from a frame.
+-- |
+-- | ```purescript
+-- | piece :: Scene (SceneI Unit Unit) FFIAudio (Effect Unit) Frame0
+-- | piece =
+-- |   Ix.do
+-- |     start
+-- |     { time } <- env -- get the environment
+-- |     create (scene time) $> Right unit
+-- |     @> loop
+-- |         ( const
+-- |             $ Ix.do
+-- |                 { time } <- env
+-- |                 ivoid $ change (scene time)
+-- |         )
+-- | ```
 env ::
   forall env audio engine proof m i.
   Monad m =>
@@ -195,6 +307,61 @@ env ::
   FrameT env audio engine proof m i i env
 env = unsafeFrame (gets _.env)
 
+-- | Get the proof term from a frame. Useful to construct a new frame using `withProof`.
+-- | The following snippet is taken from the WTK example where `proof` is used to generate a proof term that is then consumed by `withProof` in order to make the final `Frame`.
+-- |
+-- | ```purescript
+-- | piece :: { makeRenderingEnv :: MakeRenderingEnv } -> Scene (SceneI Trigger Unit) FFIAudio (Effect Unit) Frame0
+-- | piece { makeRenderingEnv } =
+-- |   ( Ix.do
+-- |       start
+-- |       ivoid $ create $ fullKeyboard klavierIdentity
+-- |       k0 <- cursor $ cursors.k0
+-- |       k1 <- cursor $ cursors.k1
+-- |       k2 <- cursor $ cursors.k2
+-- |       k3 <- cursor $ cursors.k3
+-- |       k4 <- cursor $ cursors.k4
+-- |       k5 <- cursor $ cursors.k5
+-- |       k6 <- cursor $ cursors.k6
+-- |       k7 <- cursor $ cursors.k7
+-- |       k8 <- cursor $ cursors.k8
+-- |       k9 <- cursor $ cursors.k9
+-- |       myProof <- proof
+-- |       withProof myProof
+-- |         $ Right
+-- |             { audioRefs: k0 /\ k1 /\ k2 /\ k3 /\ k4 /\ k5 /\ k6 /\ k7 /\ k8 /\ k9
+-- |             , currentKeys: (Nil :: (List KeyInfo))
+-- |             , availableKeys: K0 : K1 : K2 : K3 : K4 : K5 : K6 : K7 : K8 : K9 : Nil
+-- |             }
+-- |   )
+-- |     @> loop
+-- |         ( \{ audioRefs, currentKeys, availableKeys } -> Ix.do
+-- |             { time, trigger, active } <- env
+-- |             graphProxy <- graph
+-- |             let
+-- |               { notesOff
+-- |               , onsets
+-- |               , newCurrentKeys
+-- |               , newAvailableKeys
+-- |               , futureCurrentKeys
+-- |               , futureAvailableKeys
+-- |               } = makeRenderingEnv active trigger time availableKeys currentKeys
+-- |             ( playKeys
+-- |                 { graphProxy
+-- |                 , audioRefs
+-- |                 , currentTime: time
+-- |                 , notesOff
+-- |                 }
+-- |                 unit
+-- |                 onsets
+-- |                 newCurrentKeys
+-- |             )
+-- |               $> { audioRefs
+-- |                 , currentKeys: futureCurrentKeys
+-- |                 , availableKeys: futureAvailableKeys
+-- |                 }
+-- |         )
+-- | ```
 proof ::
   forall env audio engine proof m i.
   Monad m =>
@@ -202,6 +369,8 @@ proof ::
   FrameT env audio engine proof m i i proof
 proof = unsafeFrame (pure (unsafeCoerce unit))
 
+-- | Consumes a `proof` term to construct a `FrameT`.
+-- | This pattern is used because `FrameT` does not implement `IxApplicative`. Instead, in order to construct a frame, one needs to provide `proof` that one is at the current moment in time. This is to prevent frames from different timestamps from mixing.
 withProof ::
   forall env audio engine proof m i a.
   Monad m =>
@@ -209,6 +378,7 @@ withProof ::
   proof -> a -> FrameT env audio engine proof m i i a
 withProof _ a = unsafeFrame (pure a)
 
+-- | Get the current universe as a proxy.
 universe ::
   forall env audio engine proof m i.
   Monad m =>
@@ -216,6 +386,7 @@ universe ::
   FrameT env audio engine proof m i i (Proxy i)
 universe = unsafeFrame $ pure $ (Proxy :: _ i)
 
+-- | Get the current graph as a proxy.
 graph ::
   forall env audio engine proof m currentIdx graph changeBit skolems.
   Monad m =>
@@ -226,6 +397,7 @@ graph ::
     (Proxy graph)
 graph = unsafeFrame $ pure $ (Proxy :: _ graph)
 
+-- | Lift a computation from the underlying monad `m` into `FrameT`.
 lift ::
   forall env audio engine proof m i a.
   Monad m =>
