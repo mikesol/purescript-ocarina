24f30ce6dbc6fa510232d95cc0f1950f76230103
Mike Solomon <mike@meeshkan.com>
2021-04-20 20:07:01 +0300

    Adds waveshaper

--- examples/kitchen-sink/KitchenSink.purs
+++ examples/kitchen-sink/KitchenSink.purs
@@ -1,11 +1,12 @@
 module WAGS.Example.KitchenSink where
 
 import Prelude
-
 import Control.Comonad.Cofree (Cofree, mkCofree)
 import Control.Promise (toAffE)
 import Data.Foldable (for_)
+import Data.Int (toNumber)
 import Data.Maybe (Maybe(..))
+import Data.Array ((..))
 import Data.Vec ((+>), empty)
 import Effect (Effect)
 import Effect.Aff.Class (class MonadAff)
@@ -17,9 +18,25 @@ import Halogen.Aff (awaitBody, runHalogenAff)
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
+import Math (abs, pi)
 import WAGS.Example.KitchenSink.TLP (piece)
-import WAGS.Interpret (AudioContext, FFIAudio(..), close, context, decodeAudioDataFromUri, defaultFFIAudio, makePeriodicWave, makeUnitCache)
+import WAGS.Interpret (AudioContext, FFIAudio(..), close, context, decodeAudioDataFromUri, defaultFFIAudio, makeFloatArray, makePeriodicWave, makeUnitCache)
 import WAGS.Run (run)
+
+makeDistortionCurve :: Number -> Array Number
+makeDistortionCurve k =
+  map
+    ( \i ->
+        let
+          x = (toNumber i * 2.0 / toNumber n_samples) - 1.0
+        in
+          (3.0 + k) * x * 20.0 * deg / (pi + (k * abs x))
+    )
+    (0 .. (n_samples - 1))
+  where
+  n_samples = 44100
+
+  deg = pi / 180.0
 
 easingAlgorithm :: Cofree ((->) Int) Int
 easingAlgorithm = let fOf initialTime = mkCofree initialTime \adj -> fOf $ max 10 (initialTime - adj) in fOf 20
@@ -75,6 +92,7 @@ handleAction = case _ of
     myWave <-
       H.liftEffect
         $ makePeriodicWave audioCtx (0.3 +> -0.1 +> empty) (-0.25 +> 0.05 +> empty)
+    wicked <- H.liftEffect $ makeFloatArray (makeDistortionCurve 400.0)
     chimes <-
       H.liftAff $ toAffE
         $ decodeAudioDataFromUri
@@ -85,6 +103,7 @@ handleAction = case _ of
         (defaultFFIAudio audioCtx unitCache)
           { periodicWaves = O.singleton "my-wave" myWave
           , buffers = O.singleton "my-buffer" chimes
+          , floatArrays = O.singleton "my-waveshaper" wicked
           }
     unsubscribe <-
       H.liftEffect
