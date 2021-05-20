2cd8a06263e86aa735ed250c311a339ddccc737d
Mike Solomon <mike@meeshkan.com>
2021-05-20 15:26:15 +0300

    Adds instances for audio params

--- src/WAGS/Graph/Parameter.purs
+++ src/WAGS/Graph/Parameter.purs
@@ -15,6 +15,31 @@ derive newtype instance eqAudioParameter :: Eq AudioParameter
 
 derive newtype instance showAudioParameter :: Show AudioParameter
 
+uop :: (Number -> Number) -> AudioParameter -> AudioParameter
+uop f (AudioParameter a0@{ param: param0, timeOffset: timeOffset0, transition: transition0 }) = AudioParameter { param: f param0, timeOffset: timeOffset0, transition: transition0 }
+
+bop :: (Number -> Number -> Number) -> AudioParameter -> AudioParameter -> AudioParameter
+bop f (AudioParameter a0@{ param: param0, timeOffset: timeOffset0, transition: transition0 }) (AudioParameter a1@{ param: param1, timeOffset: timeOffset1, transition: transition1 }) = AudioParameter { param: f param0 param1, timeOffset: timeOffset0 + timeOffset1 / 2.0, transition: transition0 <> transition1 }
+
+instance semiringAudioParameter :: Semiring AudioParameter where
+  zero = param zero
+  one = param one
+  add = bop add
+  mul = bop mul
+
+instance ringAudioParameter :: Ring AudioParameter where
+  sub = bop sub
+
+instance divisionRingAudioParameter :: DivisionRing AudioParameter where
+  recip = uop recip
+
+instance commutativeRingAudioParameter :: CommutativeRing AudioParameter
+
+instance euclideanRingAudioParameter :: EuclideanRing AudioParameter where
+  degree (AudioParameter { param: param0 }) = degree param0
+  div = bop div
+  mod = bop mod
+
 -- | A control-rate audio parameter.
 -- |
 -- | `param`: The parameter as a floating-point value.
@@ -43,6 +68,15 @@ derive instance genericAudioParameterTransition :: Generic AudioParameterTransit
 
 instance showAudioParameterTransition :: Show AudioParameterTransition where
   show = genericShow
+
+instance semigroupAudioParameterTransition :: Semigroup AudioParameterTransition where
+  append Immediately _ = Immediately
+  append _ Immediately = Immediately
+  append ExponentialRamp _ = ExponentialRamp
+  append _ ExponentialRamp = ExponentialRamp
+  append LinearRamp _ = LinearRamp
+  append _ LinearRamp = LinearRamp
+  append _ _ = NoRamp
 
 -- | Create an audio parameter from a number using default parameters.
 param :: Number -> AudioParameter
