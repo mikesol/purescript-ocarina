b7130d6d192f1326eea8d2075dfdf2418666527a
Mike Solomon <mike@meeshkan.com>
2021-09-01 11:31:14 +0300

    Adds utility functions for modifying records

--- src/WAGS/Assets.purs
+++ src/WAGS/Assets.purs
@@ -1,6 +1,8 @@
 module WAGS.Assets where
 
 import Prim.Row (class Cons)
+import Record as Record
+import Type.Proxy (Proxy(..))
 
 type Analysers
   = "analysers"
@@ -19,6 +21,37 @@ type FloatArrays
 
 type Worklets
   = "worklets"
+
+analysersP :: Proxy Analysers
+analysersP = Proxy
+
+buffersP :: Proxy Buffers
+buffersP = Proxy
+
+periodicWavesP :: Proxy PeriodicWaves
+periodicWavesP = Proxy
+
+recordersP :: Proxy Recorders
+recordersP = Proxy
+
+floatArraysP :: Proxy FloatArrays
+floatArraysP = Proxy
+
+workletsP :: Proxy Worklets
+workletsP = Proxy
+
+withAnalysers :: forall r old new. (old -> new) -> { analysers :: old | r } -> { analysers :: new | r }
+withAnalysers = Record.modify analysersP
+withBuffers :: forall r old new. (old -> new) -> { buffers :: old | r } -> { buffers :: new | r }
+withBuffers = Record.modify buffersP
+withPeriodicWaves :: forall r old new. (old -> new) -> { periodicWaves :: old | r } -> { periodicWaves :: new | r }
+withPeriodicWaves = Record.modify periodicWavesP
+withRecorders :: forall r old new. (old -> new) -> { recorders :: old | r } -> { recorders :: new | r }
+withRecorders = Record.modify recordersP
+withFloatArrays :: forall r old new. (old -> new) -> { floatArrays :: old | r } -> { floatArrays :: new | r }
+withFloatArrays = Record.modify floatArraysP
+withWorklets :: forall r old new. (old -> new) -> { worklets :: old | r } -> { worklets :: new | r }
+withWorklets = Record.modify workletsP
 
 class AssetsHaveCons (key :: Symbol) (sym :: Symbol) (assets :: Row Type) (v :: Type) | key sym assets -> v
 
