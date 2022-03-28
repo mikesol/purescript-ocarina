4c5bd76cdbac8187b67dcbb43210248c6199214d
Justin Garcia <purefunctor@gmail.com>
2022-03-29 00:27:38 +0800

    Housekeeping changes (#48)
    * Allow specific examples to be compiled
    
    * Update build scripts
    
    * Remove comment from packages.dhall
    
    * Remove psci-support dependencies

--- build_all_examples.py
+++ build_all_examples.py
@@ -3,8 +3,9 @@ import subprocess
 examples = os.listdir('examples')
 
 def as_ps(name):
-  return ''.join([x[0].capitalize()+x[1:] for x in name.split('-')])
+  return ''.join(x.title() for x in name.split('-'))
 
 for example in examples:
   subprocess.run(
-      'npx spago -x examples.dhall bundle-app --main WAGS.Example.%s --to examples/%s/index.js' % (as_ps(example), example), shell=True, check=True)
+    f'EXAMPLE={example} spago -x examples.dhall bundle-app --main WAGS.Example.{as_ps(example)} --to examples/{example}/index.js', shell=True, check=True
+  )
