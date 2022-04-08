161d25d5befe032913c05ec48fe7dfff03816c2e
Mike Solomon <mike@wavr.so>
2022-04-08 16:20:42 +0300

    Better memo test

--- test/Main.purs
+++ test/Main.purs
@@ -6,6 +6,7 @@ import Control.Alt ((<|>))
 import Control.Plus (empty)
 import Data.Array (cons)
 import Data.Filterable (filter)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
@@ -87,7 +88,7 @@ main = do
               runMMZ p 0 altr
           o <- liftEffect $ Ref.read rf
           o `shouldEqual` [10,1]
-        it "should handle fold" do
+        it "should handle fold 0" do
           rf <- liftEffect $ Ref.new []
           liftEffect do
             encapsulateMMZ \p r -> do
@@ -107,3 +108,23 @@ main = do
               Ref.write [] rf
               runMMZ p unit altr
               Ref.read rf >>= shouldEqual [12,3]
+        it "should handle fold 1" do
+          rf <- liftEffect $ Ref.new []
+          liftEffect do
+            encapsulateMMZ \p r -> do
+              let add1 = map (add 1) r
+              let add2 = map (add 2) add1
+              let add3 = map (add 3) add2
+              let foldy = fold (\a b -> a + b) add3 0
+              let add4 = map (add 4) add3
+              let altr = foldy <|> add2 <|> empty <|> add4 <|> empty
+              let fm = Tuple <$> add2 <*> filter (_ > 5) altr
+              addSubscription (\i -> Ref.modify_ (cons i) rf) fm
+              runMMZ p 0 altr
+              Ref.read rf >>= shouldEqual [Tuple 3 10, Tuple 3 6]
+              Ref.write [] rf
+              runMMZ p 0 altr
+              Ref.read rf >>= shouldEqual [Tuple 3 10, Tuple 3 12]
+              Ref.write [] rf
+              runMMZ p 0 altr
+              Ref.read rf >>= shouldEqual [Tuple 3 10, Tuple 3 18]
