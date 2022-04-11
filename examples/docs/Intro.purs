804caf9faaa5a118ded625f3a0e8207652aa0fef
Mike Solomon <mike120982@gmail.com>
2022-04-11 21:26:23 +0300

    Adds menu bar

--- examples/docs/Intro.purs
+++ examples/docs/Intro.purs
@@ -29,7 +29,7 @@ px = Proxy :: Proxy """<div>
 
   <p>As audio projects become more and more ambitious, like <a href="">video games</a>, <a href="">interactive artwork</a>, <a href="">instruments</a>, <a href="">long-form pieces</a> and <a href="">live coding</a>, a need emerges for powerful abstractions to handle events and scheduling.</p>
 
-  <p>Wags's goal is to be the scheduling layer that Web Audio lacks. In doing so, it aims to be concise, expressive, and as fast as manually-optimized hand-written JavaScript. In the best of cases, it will lead you to ask and answer questions that will result in creative work you wouldn't have otherwise made.</p>
+  <p>Wags's goal is to be the scheduling layer that Web Audio lacks. In doing so, it aims to be concise, expressive, and as fast as manually-optimized hand-written JavaScript. In the best of cases, it will lead you to ask and answer questions about the flow of time that will result in creative work you wouldn't have otherwise made.</p>
 
   <h2>How does it sound?</h2>
 
@@ -37,7 +37,7 @@ px = Proxy :: Proxy """<div>
 
   <p>By the end of this documentation, you'll know all of the concepts you need to create snippets like this. This is here to illustrated the following ideas:
   <ul>
-    <li>Wags sticks to vanilla PureScript primitives and patterns, which allows you to build powerful abstractions.</li>
+    <li>Wags sticks to vanilla PureScript primitives and patterns.</li>
     <li>Wags is performant, with no audible lag on mobile devices.</li>
     <li>Wags is concise, which allows you to move at the speed of your ideas.</li>
   </ul></p>
